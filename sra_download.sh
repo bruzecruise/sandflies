3598b0a300379a3e88b56584fcb0f7b46e7787de
Dan Bruzzese <31770882+bruzecruise@users.noreply.github.com>
2022-06-24 14:03:40 -0400

    Create sra_download.sh
    # this loop cats file then for each line in the file 
    # prefetch, download fastq, gzip read1 and read2, and remove SRR file

---
+++ sra_download.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#$ -M mhuang6@nd.edu
+#$ -m abe
+#$ -r y
+#$ -pe smp 24
+#$ -q long
+#$ -N sre
+
+conda activate SRAtools
+
+DIR="/afs/crc.nd.edu/group/Leishlab/UpdatedPopGen"
+THREAD= "24"
+
+
+# this loop cats file then for each line in the file 
+# prefetch, download fastq, gzip read1 and read2, and remove SRR file
+
+cat SRA_download.txt | while read line;
+do;
+   prefetch ${line};
+   fasterq-dump --threads 12 ${line};
+   gzip ${line}_*;
+   rm ${line};
+done
+
+echo "woo done!"
